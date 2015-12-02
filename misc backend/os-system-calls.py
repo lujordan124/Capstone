50daa5d47c6f72851923d5ab4716cba31460a674
Michael <mzc2fd@virginia.edu>
2015-12-01 23:32:42 -0500

    initial test harness

--- misc backend/os-system-calls.py
+++ misc backend/os-system-calls.py
@@ -1,9 +1,27 @@
 import os
-
-if (os.name == 'nt'):
-	os.system("C:\\Users\\Michael\\Desktop\\tools\\PuTTy.lnk")
-	print 'windows'
-elif (os.name == 'posix'):
-	os.system("")
-else:
-	print 'os not supported'
+import sys
+	
+	
+	
+if __name__ == "__main__":
+	num_args = len(sys.argv)
+	if num_args == 2: # this file and param file
+		if (os.name == 'nt'):
+			os.system("echo hi")
+			print 'windows'
+		elif (os.name == 'posix'):
+			os.system("gcc " + sys.argv[1])
+			os.system("./a.out")
+		else:
+			print 'os not supported'
+	elif num_args == 3: # this file and 2 param files
+		if (os.name == 'nt'):
+			os.system("echo hello")
+			print 'windows'
+		elif (os.name == 'posix'):
+			os.system("gcc " + sys.argv[1] + " " + sys.argv[2])
+			os.system("./a.out")
+		else:
+			print 'os not supported'
+	else:
+		printf 'too many file params'
