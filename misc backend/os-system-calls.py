6bea49894b0cdee0c4cbbb84e87cc7b00afc6904
Michael Chen <mzc2fd@virginia.edu>
2016-02-25 21:41:07 -0500

    incorporated return codes

--- misc backend/os-system-calls.py
+++ misc backend/os-system-calls.py
@@ -12,15 +12,18 @@ if __name__ == "__main__":
 	output_data['standard_error'] = ''
 	if num_args == 2: # this file and param file
 		try:
+			#
+			# First attempt to compile
+			#
 			compile_process = subprocess.Popen(['gcc', sys.argv[1]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			out, err = compile_process.communicate()
-			# print 'hello!'
-			# print err
-			# print 'goodbye!'
-			if not err == "":
+			rc = compile_process.returncode
+			print 'return code is: ' + str(rc)
+			if not rc == 0:
 				raise Exception
 			output_data['compile_message'] = 'program compiled successfully!'
 
+			# Then attempt to run
 			try:
 				sp = subprocess.Popen([command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 				try:
@@ -33,22 +36,24 @@ if __name__ == "__main__":
 					output_data['standard_out'] = out
 					output_data['standard_error'] = err
 			except Exception as e:
-				output_data['standard_out'] = 'run problem' # something wrong with running the command
+				output_data['standard_out'] = 'run problem'
 		except Exception as e: 
-			output_data['compile_message'] = 'compile problem' # currently both gcc not found errors and actual compile errors both end here, need to change later
+			output_data['compile_message'] = 'compile problem'
 
 	elif num_args == 3: # this file and 2 param files
 		try:
-			sp = subprocess.Popen(['gcc', sys.argv[1], sys.argv[2]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			sp.wait(timeout=10)
+			#
+			# First attempt to compile
+			#
+			compile_process = subprocess.Popen(['gcc', sys.argv[1], sys.argv[2]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			out, err = compile_process.communicate()
+			rc = compile_process.returncode
+			print 'return code is: ' + str(rc)
+			if not rc == 0:
+				raise Exception
 			output_data['compile_message'] = 'program compiled successfully!'
 
-			# compile_process = subprocess.Popen(['gcc', sys.argv[1], sys.argv[2]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			# out, err = compile_process.communicate()
-			# if not err == "": this isn't working as of now, b/c warnings also raise exceptions, need to find solution
-			# 	raise Exception
-			# output_data['compile_message'] = 'program compiled successfully!'
-
+			# Then attempt to run
 			try:
 				sp = subprocess.Popen([command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 				try:
