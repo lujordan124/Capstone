b1cd3829707963dce08c2eea5a35a300ebfcec0a
Michael Chen <mzc2fd@virginia.edu>
2016-03-07 03:59:43 -0500

    updated UI and functionality of createQuiz frontend

--- misc backend/js/app.js
+++ misc backend/js/app.js
@@ -1,4 +1,4 @@
-var mainApp = angular.module("mainApp", ['ngRoute']);
+var mainApp = angular.module("mainApp", []);
          
 mainApp.controller("HelloController", function($scope) {
 	$scope.quiz = {};
@@ -16,10 +16,18 @@ mainApp.controller("HelloController", function($scope) {
 	$scope.quiz.endDates = [];
 	$scope.quiz.lateTimes = [];
 	$scope.quiz.lateDates = [];
-	$scope.quiz.sectionNumber = [];
-	$scope.q = "";
-	$scope.a = "";
-	$scope.n;
+	$scope.quiz.sectionNumbers = [];
+	$scope.question = "";
+	$scope.answer = "";
+	$scope.submissions;
+	$scope.part1 = true;
+	$scope.startD;
+	$scope.endD;
+	$scope.lateD;
+	$scope.startT;
+	$scope.endT;
+	$scope.lateT;
+	$scope.sectionNum;
 
 
 	$scope.add = function (q, a, n) {
@@ -34,12 +42,20 @@ mainApp.controller("HelloController", function($scope) {
 		$scope.quiz.answers.pop();
 		$scope.quiz.numSubmission.pop();
 	};
+
+	$scope.showDetails = function() {
+		$scope.part1 = false;
+	}
+
+	$scope.showQuestions = function() {
+		$scope.part1 = true;
+	}
 	
   $scope.createTest = function() {
 		if (!$scope.testCreationForm.$valid) {
 			alert("you have empty questions!!");
 		} else {
-			$http.post('url', $scope.quiz)
+			$http.post('createQuiz.php', $scope.quiz)
 				.success(function(data) { // data consists of json data returned from server side
 					$scope.quiz = {}; // clear the form so our user is ready to enter another
 					console.log(data);
@@ -50,28 +66,24 @@ mainApp.controller("HelloController", function($scope) {
 		}
   };
 
-	$scope.addStartTime = function () {
-	  $scope.quiz.startTimes.push(new Date());
+	$scope.addTime = function (startT, endT, lateT, startD, endD, lateD, sectionNum) {
+	  $scope.quiz.startTimes.push(startT);
+	  $scope.quiz.endTimes.push(endT);
+	  $scope.quiz.lateTimes.push(lateT);
+	  $scope.quiz.startDates.push(startD);
+	  $scope.quiz.endDates.push(endD);
+	  $scope.quiz.lateDates.push(lateD);
+	  $scope.quiz.sectionNumbers.push(sectionNum);
 	};
 
-	$scope.addEndTime = function () {
-	  $scope.quiz.endTimes.push(new Date());
-	};
-
-	$scope.addLateTime = function () {
-	  $scope.quiz.lateTimes.push(new Date());
-	};
-
-	$scope.addStartDate = function () {
-	  $scope.quiz.startDates.push(new Date());
-	};
-
-	$scope.addEndDate = function () {
-	  $scope.quiz.endDates.push(new Date());
-	};
-
-	$scope.addLateDate = function () {
-	  $scope.quiz.lateDates.push(new Date());
-	};
+	$scope.removeTime = function () {
+	  $scope.quiz.startTimes.pop();
+	  $scope.quiz.endTimes.pop();
+	  $scope.quiz.lateTimes.pop();
+	  $scope.quiz.startDates.pop();
+	  $scope.quiz.endDates.pop();
+	  $scope.quiz.lateDates.pop();
+	  $scope.quiz.sectionNumbers.pop();
+	}
 
 });
