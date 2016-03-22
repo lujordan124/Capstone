75e7efdc39367235da4d74dab1c94bcb389f40eb
kyq3ar <kyq3ar@virginia.edu>
2016-03-21 21:52:51 -0400

    More minor changes.

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 //force full screen
 function requestFullScreen(element) {
-	getQuestions();
-	if (checkVar == 1) {
+	if (getQuestions() == 1) {
 		document.getElementById("problemNum").innerHTML = "Problem #" + questions[qIndex].number;
 		document.getElementById("description").innerHTML = questions[qIndex].description;
 		quizID = document.getElementById("quizIDInput").value;
