42c812b42d148e97c9cddd348d316a84e7322098
Michael Chen <mzc2fd@virginia.edu>
2016-04-01 02:53:38 -0400

    restructuring complete

--- misc backend/os-system-calls.py
+++ misc backend/os-system-calls.py
@@ -5,36 +5,37 @@ import json
 
 class Compiler:
 
-	def __init__(self, student_file):
+	def __init__(self, args):
 		self.output_data = {}
 		self.output_data.setdefault('standard_out', '')
 		self.output_data.setdefault('standard_error', '')
-		self.output_file = student_file + '.out'
+		self.args = args
+		self.output_file = args[1] + '.out'
 
-	def compile(self, args=None):
-		if len(args) == 3:
+	def compile(self):
+		if len(self.args) == 3:
 			try:
-				args = ['gcc', '-o', output_file, args[1], args[2]]
+				args = ['gcc', '-o', self.output_file, self.args[1], self.args[2]]
 				compile_process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 				out, err = compile_process.communicate()
 				rc = compile_process.returncode
 				print 'return code is: ' + str(rc)
 				if rc != 0:
 					raise Exception
-				output_data['compile_message'] = 'program compiled successfully!'
+				self.output_data['compile_message'] = 'program compiled successfully!'
 			except Exception as e:
 				self.output_data['compile_message'] = 'There was a compilation problem.'
 				print e
-		elif len(args) == 4:
+		elif len(self.args) == 4:
 			try:
-				args = ['gcc', '-o', output_file, args[1], args[2]]
+				args = ['gcc', '-o', self.output_file, self.args[1], self.args[2]]
 				compile_process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 				out, err = compile_process.communicate()
 				rc = compile_process.returncode
 				print 'return code is: ' + str(rc)
 				if rc != 0:
 					raise Exception
-				output_data['compile_message'] = 'Program compiled successfully!'
+				self.output_data['compile_message'] = 'Program compiled successfully!'
 			except Exception as e:
 				self.output_data['compile_message'] = 'There was a compilation problem.'
 				print e
@@ -43,7 +44,7 @@ class Compiler:
 
 	def run(self):
 		try:
-			command = './' + output_file
+			command = './' + self.output_file
 			sp = subprocess.Popen([command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 			try:
 				out, err = sp.communicate()
@@ -55,20 +56,20 @@ class Compiler:
 				self.output_data['standard_out'] = out
 				self.output_data['standard_error'] = err
 		except Exception as e:
+			print 'hello'
 			self.output_data['standard_out'] = 'Run problem. Maybe the file did not compile?'
 
 	def clean(self):
-		rc = os.remove(self.output_file)
-		print 'remove rc: ' + str(rc)
-		self.output_file = sys.argv[1] + '.txt'
+		os.remove(self.output_file)
+		self.output_file = self.args[1] + '.txt'
 		with open(self.output_file, 'w') as outfile:
-			json.dump(output_data, outfile)
+			json.dump(self.output_data, outfile)
 		# a+rwx
 		os.chmod(self.output_file, 0777)
 	
 if __name__ == "__main__":
 	os.putenv("TMPDIR", "/tmp")
-	compiler = Compiler(sys.argv[1])
-	compiler.compile(sys.argv)
+	compiler = Compiler(sys.argv)
+	compiler.compile()
 	compiler.run()
 	compiler.clean()
